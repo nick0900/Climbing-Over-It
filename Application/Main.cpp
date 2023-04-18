2b98876d72900eef9bced8834e4d32bf6d2f28bb
nick0900 <nicolas.angnell@hotmail.se>
2023-04-18 10:54:12 +0200

    feat: box2D added and verified. hello cube scene done.

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <thread>
 #include <string>
-#include <Windows.h>
+//#include <Windows.h>
 
-#include "lua.hpp"
+//#include "lua.hpp"
 
+/*
 void DumpErreor(lua_State* L)
 {
 	if (lua_gettop(L) && lua_isstring(L, -1))
@@ -45,4 +46,161 @@ int main()
 	}
 
 	return 0;
+}
+*/
+
+/*******************************************************************************************
+*
+*   raylib [core] example - Initialize 3d camera free
+*
+*   Example originally created with raylib 1.3, last time updated with raylib 1.3
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2015-2023 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include "box2d.h"
+#include <chrono>
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 2048;
+    const int screenHeight = 2048;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
+
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = Vector3{ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = Vector3{ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = Vector3{ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+    Vector3 cubePosition = { 0.0f, 4.0f, 0.0f };
+
+    DisableCursor();                    // Limit cursor to relative movement inside the window
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+
+
+    //-------------------------------Box2D setup--------------------------------------------
+    b2Vec2 gravity(0.0f, -10.0f);
+
+    b2World world(gravity);
+
+
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f, -10.0f);
+
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    b2PolygonShape groundBox;
+    groundBox.SetAsBox(50.0f, 10.0f);
+
+    groundBody->CreateFixture(&groundBox, 0.0f);
+
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f, 4.0f);
+    b2Body* body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+
+    body->CreateFixture(&fixtureDef);
+
+    float timestep = 1.0f / 60.0f;
+
+    std::chrono::duration<double> timestepDur(timestep);
+
+    std::chrono::steady_clock::time_point prev = std::chrono::steady_clock::now();
+    std::chrono::duration<double> stepTimer;
+    //--------------------------------------------------------------------------------------
+
+
+
+
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera, CAMERA_FREE);
+
+        if (IsKeyDown('Z')) camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
+
+
+        std::chrono::steady_clock::time_point curr = std::chrono::steady_clock::now();
+        stepTimer = std::chrono::duration_cast<std::chrono::duration<double>>(curr - prev);
+        prev = curr;
+
+        if (stepTimer >= timestepDur)
+        {
+            stepTimer -= timestepDur;
+
+            world.Step(timestep, 6, 2);
+
+            b2Vec2 position = body->GetPosition();
+            float angle = body->GetAngle();
+
+            cubePosition = { position.x, position.y, 0.0f };
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+
+        DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+        DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+        //DrawCube({0.0f, -10.0f, 0.0f}, 100.0f, 20.0f, 2.0f, BLUE);
+        DrawCubeWires({ 0.0f, -10.0f, 0.0f }, 100.0f, 20.0f, 2.0f, MAROON);
+
+        DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        DrawRectangle(10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(10, 10, 320, 133, BLUE);
+
+        DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+        DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+        DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+        DrawText("- Alt + Mouse Wheel Pressed to Rotate", 40, 80, 10, DARKGRAY);
+        DrawText("- Alt + Ctrl + Mouse Wheel Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY);
+        DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
